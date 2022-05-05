3b0a7fccca3fcc1a2e58fcd0361f0384010fdbb6
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2022-05-05 15:55:35 -0300

    .

--- teste.py
+++ teste.py
@@ -12,11 +12,14 @@
 # Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >=235 and uf ='ce' and cep <> '62930000';
 # "from usuario Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
 
+from pickle import TRUE
 import re
 
 txt = "Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
+txt2 = "Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >= 235 and uf = ce and cep <> 62930000"
 txt = txt.lower()
-#validate = re.search("^(?=.*select.*from.)", txt)
+txt2 = txt2.lower()
+# validate = re.search("^(?=.*select.*from.)", txt)
 
 
 def camposSelect(txt):
@@ -40,7 +43,41 @@ def tabelasJoin(txt):
     return camposAux
 
 
-camposSelect = tabelaFrom(txt)
+def camposJoin(txt):
+    campos = txt.split("where")[0].split("join")
+    campos.pop(0)
+    camposAux = []
+    for c in campos:
+        campos2 = c.split(" ")
+        for i in campos2:
+            if i.find('.') != -1:
+                camposAux.append(i)
+
+    return camposAux
+
+
+def camposWhere(txt):
+    campos = txt.split("where")[1].split(" ")
+    camposAux = []
+    for i in range(len(campos)):
+        if checkOperators(campos[i]):
+            camposAux.append(campos[i-1])
+
+    return camposAux
+
+
+def checkOperators(campo):
+    if (campo.__contains__("=") |
+            campo.__contains__("<") |
+            campo.__contains__(">") |
+            campo.__contains__("<>") |
+            campo.__contains__(">=") |
+            campo.__contains__("<=")):
+        return True
+    return False
+
+
+camposSelect = camposWhere(txt)
 
 
 print(camposSelect)
