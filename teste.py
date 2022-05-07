81aa0430a2e1847b360dc80e473e0c3ddb07842c
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2022-05-07 13:09:39 -0300

    feito validação de tabelas e campos com o banco

--- teste.py
+++ teste.py
@@ -12,75 +12,18 @@
 # Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >=235 and uf ='ce' and cep <> '62930000';
 # "from usuario Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
 
-from pickle import TRUE
 import re
+import validarCampos
 
-txt = "Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
-txt2 = "Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >= 235 and uf = ce and cep <> 62930000"
+txt = "Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição from usuario join contas on usuario.idUsuario2 = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
+txt2 = "Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >= 235 and uf = 'ce' and cep <> 62930000"
 txt = txt.lower()
 txt2 = txt2.lower()
 # validate = re.search("^(?=.*select.*from.)", txt)
 
 
-def camposSelect(txt):
-    campos = txt.split("from")[0]
-    campos = campos.replace(
-        "select", "").replace(" ", "").split(",")
-    return campos
+print(validarCampos.validar(txt))
 
-
-def tabelaFrom(txt):
-    campo = txt.split("from")[1].split(" ")[1]
-    return campo
-
-
-def tabelasJoin(txt):
-    campos = txt.split("join")
-    camposAux = []
-    for i in range(len(campos)):
-        if i > 0:
-            camposAux.append(campos[i].split(" ")[1])
-    return camposAux
-
-
-def camposJoin(txt):
-    campos = txt.split("where")[0].split("join")
-    campos.pop(0)
-    camposAux = []
-    for c in campos:
-        campos2 = c.split(" ")
-        for i in campos2:
-            if i.find('.') != -1:
-                camposAux.append(i)
-
-    return camposAux
-
-
-def camposWhere(txt):
-    campos = txt.split("where")[1].split(" ")
-    camposAux = []
-    for i in range(len(campos)):
-        if checkOperators(campos[i]):
-            camposAux.append(campos[i-1])
-
-    return camposAux
-
-
-def checkOperators(campo):
-    if (campo.__contains__("=") |
-            campo.__contains__("<") |
-            campo.__contains__(">") |
-            campo.__contains__("<>") |
-            campo.__contains__(">=") |
-            campo.__contains__("<=")):
-        return True
-    return False
-
-
-camposSelect = camposWhere(txt)
-
-
-print(camposSelect)
 
 # if validate:
 #   print("YES! We have a match!")
