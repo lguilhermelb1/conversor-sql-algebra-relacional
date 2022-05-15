932a1f071edc69c94aa528b8453a19bb559c02fd
lguilhermelb1 <luiseuguilherme@gmail.com>
2022-05-15 14:10:29 -0300

    Conversor e interface

--- conversor.py
+++ conversor.py
@@ -1,23 +1,14 @@
-from validarCampos import camposJoin, camposSelect, tabelaFrom, tabelasJoin, camposWhere, validar
+from validarCampos import camposJoin, camposSelect, tabelaFrom, tabelasJoin, camposWhere
+from validarFormatoSQL import validar_sql
+
+def converteTudo(txt):
+    return conversorSelect(txt) + " " + conversorCamposWhere(txt) + " " + conversorFrom(txt) + conversorJoinTabelas(txt)
 
 def conversorSelect(txt):
-    campos = camposSelect(txt)
-    select = ""
-    
-    for i in range(len(campos)):
-        if i == len(campos)-1:
-            select += str(campos[i]) + " "
-        else:
-            select += str(campos[i]) + ", "
-    
-    #Retorna tudo
-    return "π " + select + conversorCamposWhere(txt) + conversorFrom(txt) + conversorJoinTabelas(txt)
+    return "π " + validar_sql(txt).get('campos').get('select')
 
-def conversorFrom(txt):
-    tabela = tabelaFrom(txt)
-    tabelaaFrom = str(tabela)
-    
-    return "(" + tabelaaFrom
+def conversorFrom(txt):    
+    return "(" + validar_sql(txt).get('campos').get('from')
 
 def conversorJoinTabelas(txt):
     tabelas = tabelasJoin(txt)
@@ -43,20 +34,4 @@ def conversorJoinCampos(txt):
     return campoJoin
 
 def conversorCamposWhere(txt):
-    #Adiciona os campos a variavel
-    campos = camposWhere(txt)
-    #Auxiliar para adicionar o texto
-    campoWhere = ""
-
-    #Percorre os campos
-    for i in range(len(campos)):
-        #Adiciona ao auxiliar e trata
-        #Se for ultimo da lista campos adiciona espaço
-        if i == len(campos)-1:
-            campoWhere += str(campos[i]) + " "
-        #Se não for adiciona a vírgula
-        else:
-            campoWhere += str(campos[i]) + " AND "
-    
-    #Retorna o auxiliar preenchido e tratado
-    return "σ " + campoWhere
+    return "σ " + validar_sql(txt).get('campos').get('where')
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 import validarCampos, validarFormatoSQL, conversor
 
-txt = "Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >= 235 and uf = ce and cep <> 62930000"
+txt = "Select idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta where saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3"
+txt2 = "Select nome, datanascimento, descricao, saldoinicial from usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario where saldoinicial >= 235 and uf = ce and cep <> 62930000"
 txt = txt.lower()
 
 resultado = validarFormatoSQL.validar_sql(txt)
 validarCampos.validar(txt)
 
-print(conversor.conversorSelect(txt))
+print(conversor.converteTudo(txt))
+print("\n")
 print(resultado.get('campos'))
